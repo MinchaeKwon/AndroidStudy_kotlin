80f1fd63dc3eb8c0870f106b36411214c8da7f9d
권민채 <minchae075@naver.com>
2022-07-14 18:05:00 +0900

    add: navigation 추가

--- app/src/main/java/com/example/androidstudy_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy_kotlin/MainActivity.kt
@@ -1,30 +1,39 @@
 package com.example.androidstudy_kotlin
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.androidstudy_kotlin.databinding.ActivityMainBinding
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+//import com.example.androidstudy_kotlin.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater);
-        setContentView(binding.root);
+//        binding = ActivityMainBinding.inflate(layoutInflater);
+//        setContentView(binding.root);
 
-        binding.btnViewBiding.setOnClickListener {
-            startActivity(Intent(this, ViewActivity::class.java))
-        }
+//        navController = binding.navHostFragment.findNavController()
+        navController = nav_host_fragment.findNavController()
 
-        binding.btnDataBinding.setOnClickListener {
-            startActivity(Intent(this, DataActivity::class.java));
-        }
-
-        binding.btnRoom.setOnClickListener {
-            startActivity(Intent(this, RoomActivity::class.java))
-        }
+//        binding.btnViewBiding.setOnClickListener {
+//            startActivity(Intent(this, ViewActivity::class.java))
+//        }
+//
+//        binding.btnDataBinding.setOnClickListener {
+//            startActivity(Intent(this, DataActivity::class.java));
+//        }
+//
+//        binding.btnRoom.setOnClickListener {
+//            startActivity(Intent(this, RoomActivity::class.java))
+//        }
+//
+//        navController = binding.btnNav.findNavController()
     }
 }
---
+++ app/src/main/java/com/example/androidstudy_kotlin/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.example.androidstudy_kotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+
+class MainFragment : Fragment() {
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        btn_nav.setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_firstFragment)
+        }
+    }
+}
