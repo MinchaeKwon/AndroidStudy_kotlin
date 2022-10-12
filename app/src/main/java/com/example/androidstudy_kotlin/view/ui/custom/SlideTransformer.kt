b168d9a92e7aac05dc5905f1aeb6ff2205dde50e
권민채 <minchae075@naver.com>
2022-10-12 17:40:04 +0900

    cardview에 elevation 적용

--- app/src/main/java/com/example/androidstudy_kotlin/view/ui/custom/SlideTransformer.kt
+++ app/src/main/java/com/example/androidstudy_kotlin/view/ui/custom/SlideTransformer.kt
@@ -5,18 +5,7 @@ import androidx.core.view.ViewCompat
 import androidx.viewpager2.widget.ViewPager2
 import java.lang.Math.abs
 
-
 class SlideTransformer(private val offscreenPageLimit: Int): ViewPager2.PageTransformer {
-    companion object {
-        private const val DEFAULT_TRANSLATION_X = .0f
-        private const val DEFAULT_TRANSLATION_FACTOR = 1.2f
-
-        private const val SCALE_FACTOR = .14f
-        private const val DEFAULT_SCALE = 1f
-
-        private const val ALPHA_FACTOR = .3f
-        private const val DEFAULT_ALPHA = 1f
-    }
 
     private val MIN_SCALE = 0.5f
     private val MAX_SCALE = 0.8f
@@ -61,33 +50,6 @@ class SlideTransformer(private val offscreenPageLimit: Int): ViewPager2.PageTran
                     alpha = MIN_FADE
                 }
             }
-
-
-//            ViewCompat.setElevation(page, -abs(position))
-//
-//            val scaleFactor = -SCALE_FACTOR * position + DEFAULT_SCALE
-//            val alphaFactor = -ALPHA_FACTOR * position + DEFAULT_ALPHA
-//
-//            when {
-//                position <= 0f -> {
-//                    translationX = DEFAULT_TRANSLATION_X
-//                    scaleX = DEFAULT_SCALE
-//                    scaleY = DEFAULT_SCALE
-//                    alpha = DEFAULT_ALPHA + position
-//                }
-//                position <= offscreenPageLimit - 1 -> {
-//                    scaleX = scaleFactor
-//                    scaleY = scaleFactor
-//                    translationX = -(width / DEFAULT_TRANSLATION_FACTOR) * position
-//                    alpha = alphaFactor
-//                }
-//                else -> {
-//                    translationX = DEFAULT_TRANSLATION_X
-//                    scaleX = DEFAULT_SCALE
-//                    scaleY = DEFAULT_SCALE
-//                    alpha = DEFAULT_ALPHA
-//                }
-//            }
         }
     }
 }
